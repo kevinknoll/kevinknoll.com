12c5e777bb76714c45012d3212860d7f9b0d0dea
Kevin Knoll <knollkevin@gmail.com>
2019-03-21 22:12:58 -0400

    Add first blog post

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,22 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("web/sw.js");
   eleventyConfig.addPassthroughCopy("web/css");
 
+  // datetime filter to format dates and times
+  // takes a 'format' parameter that mimics moment.js
+  // see https://momentjs.com/docs/#/displaying/
+  eleventyConfig.addFilter('dt', function(dt, format) {
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'];
+
+    return format
+      .replace('YYYY', dt.getUTCFullYear())
+      .replace('MMMM', months[dt.getUTCMonth()])
+      .replace('MM', ('0' + (dt.getUTCMonth() + 1)).slice(-2))
+      .replace('DD', ('0' + dt.getUTCDate()).slice(-2))
+      .replace('HH', ('0' + dt.getUTCHours()).slice(-2))
+      .replace('mm', ('0' + dt.getUTCMinutes()).slice(-2))
+      .replace('ss', ('0' + dt.getUTCSeconds()).slice(-2))
+  });
+
   return {
     passthroughFileCopy: true
   };
