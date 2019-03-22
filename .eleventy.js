ebc6dc1caa5ebcba5b674dc630ef6248620085a5
Kevin Knoll <knollkevin@gmail.com>
2019-03-21 23:41:58 -0400

    Add favicon and friends

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,17 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("web/sw.js");
   eleventyConfig.addPassthroughCopy("web/css");
   eleventyConfig.addPassthroughCopy("web/js");
+  eleventyConfig.addPassthroughCopy("web/android-chrome-192x192.png");
+  eleventyConfig.addPassthroughCopy("web/android-chrome-512x512.png");
+  eleventyConfig.addPassthroughCopy("web/apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("web/browserconfig.xml");
+  eleventyConfig.addPassthroughCopy("web/favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("web/favicon-32x32.png");
+  eleventyConfig.addPassthroughCopy("web/favicon.ico");
+  eleventyConfig.addPassthroughCopy("web/mstile-150x150.png");
+  eleventyConfig.addPassthroughCopy("web/safari-pinned-tab.svg");
+  eleventyConfig.addPassthroughCopy("web/site.webmanifest");
 
-  // datetime filter to format dates and times
   // takes a 'format' parameter that mimics moment.js
   // see https://momentjs.com/docs/#/displaying/
   eleventyConfig.addFilter('dt', function(dt, format) {
