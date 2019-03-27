c6c78e4e7020714bb59655632485851944868382
Kevin Knoll <knollkevin@gmail.com>
2019-03-27 00:58:12 -0400

    Replace css animation on home page with lax.js

--- web/js/home.js
+++ web/js/home.js
@@ -1,15 +1,32 @@
 (function () {
-  var intro = document.getElementById('intro');
+  var kev = document.querySelector('.kev');
+  var k = document.querySelector('.k');
+  var end = document.querySelector('.end');
+  var dev = document.querySelector('.dev');
 
-  if (!intro || !intro.classList) {
-    return;
+  function setup(selector, attr, value) {
+    var els = document.querySelectorAll(selector);
+    for (var i = 0, ii = els.length; i < ii; ++i) {
+      els[i].setAttribute('data-lax-' + attr, value);
+      els[i].setAttribute('data-lax-anchor', '#intro');
+      els[i].classList.add('lax');
+    }
   }
 
-  window.addEventListener('scroll', function (e) {
-    if (window.scrollY > 100) {
-      intro.classList.add('animate');
-    } else {
-      intro.classList.remove('animate');
-    }
-  }, false);
+  setup('.fade', 'opacity', '0 1, 100 0');
+  setup('.kev,.k', 'translate-y', '0 0, 100 50');
+  setup('.k', 'opacity', '0 0, 50 1');
+  setup('.k', 'translate-x', '0 0, 100 ' + ((kev.offsetLeft + kev.offsetWidth) - k.offsetLeft));
+  setup('.dev', 'translate-x', '0 0, 100 5');
+  setup('.end', 'translate-x', '0 0, 100 ' + (dev.offsetLeft - end.offsetLeft - 7));
+  setup('.end', 'translate-y', '0 0, 100 ' + (dev.offsetTop - end.offsetTop));
+
+  lax.setup();
+
+  function updateLax() {
+    lax.update(window.scrollY);
+    window.requestAnimationFrame(updateLax);
+  }
+
+  window.requestAnimationFrame(updateLax)
 })();
